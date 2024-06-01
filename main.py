3ec4f665d5a94eab861ce56469510a520a52ae85
G1itch <94213385+glitch65@users.noreply.github.com>
2024-06-01 19:46:29 +0900

    v0.3 release

--- main.py
+++ main.py
@@ -108,70 +108,115 @@ def_cfg = {
                 "invisible_mode": False,
                 "Enable_activity": True,
                 "Activity_type": "playing",
-                "Activity_name": "Five Nuker on TOP!"
+                "Activity_name": "Five Nuker on TOP!",
+                "Selected_theme": "default"
             }
 
-if not os.path.exists("cfg"):
-        os.mkdir("cfg")
-        with open("cfg/config.json", "w") as cfg:
-            json.dump(def_cfg,cfg,indent=3)
-        print(f"{F.YELLOW}cfg/config.json not exists, created a config file!\nPlease check and edit a cfg/config.json!{F.RESET}")
-        stop_nuker()
-if os.path.exists("cfg/config.json"):
-        try:
-            with open("cfg/config.json", "r") as cfg:
-                config = json.loads(cfg.read())
-        except Exception as e:
-            print("failed to load the config :(")
-            print(f"{e}")
-else:
-        with open("cfg/config.json", "w") as cfg:
-            json.dump(def_cfg,cfg,indent=3)
-        print(f"{F.YELLOW}cfg/config.json not exists, created a config file!\nPlease check and edit a cfg/config.json!{F.RESET}")
-        stop_nuker()
+def_theme = {
+    "logo_pallete": ((125,0,255),(0,0,0)),
+    "logo_gradient_type":"V",
+    "logo_gradient_steps":11,
+    "logged_in_pallete": ((7,227,0),(7,145,3),(6,214,0),(5,138,1),(5,102,2),(4,117,2),(4,92,2)),
+    "logged_in_gradient_type": "H",
+    "logged_in_gradient_steps": 18,
+    "invisible_mode_pallete":((163,163,163),(133,133,133),(99,99,99)),
+    "invisible_mode_type":"H",
+    "invisible_mode_steps": 21,
+    "activity_type_error_pallete": ((255,0,0),(176,0,0)),
+    "activity_type_error_gradient_type": "H",
+    "activity_type_error_steps": 191,
+    "command_triggered_pallete": ((0,255,0),(0,125,0),(0,255,0)),
+    "command_triggered_gradient_type": "H",
+    "nuke_started_pallete": ((255,0,0),(255,0,0)),
+    "nuke_started_gradient_type": "H",
+    "nuke_started_steps": 1
+}
 
-def check_cfg():
-    error = False
-    list_of_settings = ["token",
-                        "prefix",
-                        "nuke_prefix",
-                        "names_of_channels_and_roles",
-                        "name_of_webhooks",
-                        "spam_text",
-                        "spam_mode",
-                        "channels_create_count",
-                        "spam_in_channel_count",
-                        "server_name",
-                        "whitelisted_ids",
-                        "only_whitelisted_users_can_perform_actions",
-                        "Enable logging?",
-                        "Ban on server nuke?",
-                        "ban_reason",
-                        "invisible_mode",
-                        "Activity_type",
-                        "Activity_name"]
-    for setting in list_of_settings:
-            try:
-                i = config[setting]
-            except KeyError:
-                config[setting] = def_cfg[setting]
-                with open("cfg/config.json", "w") as cfg:
-                    json.dump(config,cfg,indent=3)
-                if error == False:
-                    error = True
-    if error == True:            
-        clear()
-        Center("When checking the config, some bugs were found and corrected")
-        Center("This usually happens if there are missing lines in your config. This can be caused by a nuker update.")
-        Center("It is recommended to check and change config")
-        Center("To continue press the space bar on your keyboard...")
-        keyboard.wait("space")
-        clear()
+
+
+
+
+
+if not os.path.exists("cfg"):
+            os.mkdir("cfg")
+            with open("cfg/config.json", "w") as cfg:
+                json.dump(def_cfg,cfg,indent=3)
+            print(f"{F.YELLOW}cfg/config.json not exists, created a config file!\nPlease check and edit a cfg/config.json!{F.RESET}")
+            stop_nuker()
     
-check_cfg()
+if os.path.exists("cfg/config.json"):
+    try:
+        with open("cfg/config.json", "r") as cfg:
+            config = json.loads(cfg.read())
+    except Exception as e:
+        print("failed to load the config :(")
+        print(f"{e}")
+        stop_nuker()
+else:
+                with open("cfg/config.json", "w") as cfg:
+                    json.dump(def_cfg,cfg,indent=3)
+                print(f"{F.YELLOW}cfg/config.json not exists, created a config file!\nPlease check and edit a cfg/config.json!{F.RESET}")
+                stop_nuker()
+error = False
+list_of_settings = ["token",
+                            "prefix",
+                            "nuke_prefix",
+                            "names_of_channels_and_roles",
+                            "name_of_webhooks",
+                            "spam_text",
+                            "spam_mode",
+                            "channels_create_count",
+                            "spam_in_channel_count",
+                            "server_name",
+                            "whitelisted_ids",
+                            "only_whitelisted_users_can_perform_actions",
+                            "Enable logging?",
+                            "Ban on server nuke?",
+                            "ban_reason",
+                            "invisible_mode",
+                            "Activity_type",
+                            "Activity_name",
+                            "Selected_theme"]
+for setting in list_of_settings:
+                try:
+                    i = config[setting]
+                except KeyError:
+                    config[setting] = def_cfg[setting]
+                    with open("cfg/config.json", "w") as cfg:
+                        json.dump(config,cfg,indent=3)
+                    if error == False:
+                        error = True
+if error == True:            
+            clear()
+            Center("When checking the config, some bugs were found and corrected")
+            Center("This usually happens if there are missing lines in your config. This can be caused by a nuker update.")
+            Center("It is recommended to check and change config")
+            Center("To continue press the space bar on your keyboard...")
+            keyboard.wait("space")
+            clear()
+        
+    
+    
+def themes_empty():
+    return len(os.listdir("cfg//themes")) == 0
+if not os.path.exists("cfg//themes"):
+            os.mkdir("cfg//themes")
+            with open(f"cfg/themes/default.json", "w") as theme:
+                json.dump(def_theme,theme,indent=3)
+            
+if themes_empty:
+    with open(f"cfg/themes/default.json", "w") as theme:
+                json.dump(def_theme,theme,indent=3)
+try:
+    with open(f"cfg/themes/{config["Selected_theme"]}.json", "r") as thm:
+        theme = json.loads(thm.read())
+except Exception as e:
+    print("failed to load theme :(")
+    print(f"{e}")
+    stop_nuker()        
 
 if __name__ == '__main__':
-    CenterColor(raw_image,((125,0,255),(0,0,0)), len(raw_image.split("\n")),"V")                    
+    CenterColor(raw_image,theme["logo_pallete"], theme["logo_gradient_steps"],theme["logo_gradient_type"])
     print(f"{F.GREEN}Config file loaded!{F.RESET}")
     
     if config["Enable logging?"] == False:
@@ -186,27 +231,28 @@ with open('icon.png', 'rb') as f:
 
 bot = commands.Bot(config['prefix'],intents=discord.Intents.all(),help_command=None)
 
-@bot.event
-async def on_ready():
-    clear()
-    os.system(f"title Five Nuker - Online - {bot.user} - ")
-    CenterColor(raw_image1,((125,0,255),(0,0,0)), len(raw_image.split("\n")),"V")
-    CenterColor(f"You loggen by {bot.user}",((7,227,0),(7,145,3),(6,214,0),(5,138,1),(5,102,2),(4,117,2),(4,92,2)), len(f"You loggen by {bot.user}"),"H")
-    if config["Activity_type"] == "playing" and config["invisible_mode"] == False:
-        await bot.change_presence(activity=discord.Game(name=config["Activity_name"]))
-    elif config["Activity_type"] == "listening" and config["invisible_mode"] == False:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=config["Activity_name"]))
-    elif config["Activity_type"] == "streaming" and config["invisible_mode"] == False:
-        await bot.change_presence(activity=discord.Streaming(name=config["Activity_name"], url='https://www.twitch.tv/'))
-    elif config["Activity_type"] == "watching" and config["invisible_mode"] == False:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=config["Activity_name"]))
-    elif config["Activity_type"] == None and config["invisible_mode"] == True:
-        await bot.change_presence(status=discord.Status.offline)
-        CenterColor("Invisible mode enabled!",((163,163,163),(133,133,133),(99,99,99)), 21,"H")   
-    else:
-        CenterColor("ERROR!!! Activity_type must be playing, listening, streaming, watching or null. invisible_mode must be true or false. The bot's activity will not change and invisible mode will not be enabled",((255,0,0),(176,0,0)),len("ERROR!!! Activity_type must be playing, listening, streaming, watching or null. invisible_mode must be true or false. The bot's activity will not change and invisible mode will not be enabled"),"H")
+if __name__ == '__main__':
+    @bot.event
+    async def on_ready():
+        clear()
+        os.system(f"title Five Nuker - Online - {bot.user} - ")
+        CenterColor(raw_image,theme["logo_pallete"], theme["logo_gradient_steps"],theme["logo_gradient_type"])
+        CenterColor(f"You loggen by {bot.user}",theme["logged_in_pallete"], theme["logged_in_gradient_steps"],theme["logged_in_gradient_type"])
+        if config["Activity_type"] == "playing" and config["invisible_mode"] == False:
+            await bot.change_presence(activity=discord.Game(name=config["Activity_name"]))
+        elif config["Activity_type"] == "listening" and config["invisible_mode"] == False:
+            await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=config["Activity_name"]))
+        elif config["Activity_type"] == "streaming" and config["invisible_mode"] == False:
+            await bot.change_presence(activity=discord.Streaming(name=config["Activity_name"], url='https://www.twitch.tv/'))
+        elif config["Activity_type"] == "watching" and config["invisible_mode"] == False:
+            await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=config["Activity_name"]))
+        elif config["Activity_type"] == None and config["invisible_mode"] == True:
+            await bot.change_presence(status=discord.Status.offline)
+            CenterColor("Invisible mode enabled!",theme["invisible_mode_pallete"], theme["invisible_mode_steps"],theme["invisible_mode_type"])   
+        else:
+            CenterColor("ERROR!!! Activity_type must be playing, listening, streaming, watching or null. invisible_mode must be true or false. The bot's activity will not change and invisible mode will not be enabled",theme["activity_type_error_pallete"],theme["activity_type_error_steps"],theme["activity_type_error_gradient_type"])
 
-    Center(f"Message logs:")  
+        Center(f"Message logs:")  
 
 
 
@@ -268,7 +314,7 @@ async def on_message(message: discord.Message):
         return
     msg = message.content
     if msg.startswith(config["nuke_prefix"]):
-        CenterColor(f"[{message.author}]:{msg}",((0,255,0),(0,125,0),(0,255,0)), len(f"[{message.author}]: {msg}"),"H")
+        CenterColor(f"[{message.author}]:{msg}",theme["command_triggered_pallete"], len(f"[{message.author}]: {msg}"),theme["command_triggered_gradient_type"])
         args = msg.split()
         if args[0] == config['nuke_prefix']+"nuke":
             if config['only_whitelisted_users_can_perform_actions'] == True:
@@ -277,7 +323,7 @@ async def on_message(message: discord.Message):
                     await message.guild.edit(name=config["server_name"], icon=icon)
                     spamCount = config['spam_in_channel_count']
                     channelsCreate = config['channels_create_count']
-                    CenterColor(f"Nuking a {message.guild.name}!\nSettings | SMPC (Spam Message Per Channel): {spamCount} | Channels Count: {spamCount}",((255,0,0),(255,0,0)), 1,"H")
+                    CenterColor(f"Nuking a {message.guild.name}!\nSettings | SMPC (Spam Message Per Channel): {spamCount} | Channels Count: {channelsCreate}",theme["nuke_started_pallete"], theme["nuke_started_steps"],theme["nuke_started_gradient_type"])
                     create_task(delete_channels(message.guild,))
                     create_task(delete_roles(message.guild,))
                     for i in range(channelsCreate):
@@ -289,7 +335,7 @@ async def on_message(message: discord.Message):
                 await message.guild.edit(name=config["server_name"], icon=icon)
                 spamCount = config['spam_in_channel_count']
                 channelsCreate = config['channels_create_count']
-                CenterColor(f"Nuking a {message.guild.name}!\nSettings | SMPC (Spam Message Per Channel): {spamCount} | Channels Count: {spamCount}",((255,0,0),(255,0,0)), 1,"H")
+                CenterColor(f"Nuking a {message.guild.name}!\nSettings | SMPC (Spam Message Per Channel): {spamCount} | Channels Count: {channelsCreate}",theme["nuke_started_pallete"], theme["nuke_started_steps"],theme["nuke_started_gradient_type"])
                 create_task(delete_channels(message.guild,))
                 create_task(delete_roles(message.guild,))
                 for i in range(channelsCreate):
